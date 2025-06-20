47799f04ecff675de18b006cd7dc0bfb4b766204
Agraj <agrajmbiju424@gmail.com>
2025-06-20 09:27:34 -0400

    Added full React app for Calm & Cure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Calm n Cure </h1>
+      <div className="section body">Body</div>
+
+    </div>
+  );
+};
+
+export default Home;
